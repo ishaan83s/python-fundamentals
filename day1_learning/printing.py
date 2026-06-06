3839312b7a0f7b783ba8dbe66d8b051b54cd17fc
ishaan83s <berealishaan@gmail.com>
2026-06-06 15:37:57 +0530

    Day 1 : Learning input output , variables , datatypes and  operators

---
+++ day1_learning/printing.py
@@ -0,0 +1,4 @@
+print("Hello world")
+print("Vivaan is my friend." , "He loves Asteroids.")
+print(23)
+print(35+23)
---
+++ day1_learning/variables.py
@@ -0,0 +1,24 @@
+name = "Vivaan"
+age = 67
+price = 2.99
+
+# age2 = age
+# age3 = age + 67
+
+# print("My name is", name)
+# print("I am" ,age , "years old.")
+# print("My price is", price , "rupees.")
+
+print(name)
+print(age)
+print(price)
+
+print(age + price)
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+# print(age2)
+# print(age3)
+
