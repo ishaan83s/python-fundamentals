907ea7e9b04f874fb9613a6c59f7b8394ad4bac5
ishaan83s <berealishaan@gmail.com>
2026-06-14 01:30:53 +0530

    day 7 - Started functions basics today

---
+++ day7_learning/functions_basics.py
@@ -0,0 +1,16 @@
+def calcSum(x,y):  #Function definition
+    s = x + y  #Work
+    # print(s)
+    return s #What value will it return
+
+print(calcSum(2,3)) #calling function 
+
+#AVERAGE OF 3 FUNCTIONS
+
+def calc_avg(x,y,z):
+    sum = x+y+z
+    avg = sum/3
+    print(avg)
+    return avg
+
+calc_avg(1,2,3)
