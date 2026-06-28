6056c83544f60210028abede6a4c44c72eb89e15
ishaan83s <berealishaan@gmail.com>
2026-06-28 18:52:08 +0530

    fixed some lines

--- day01_learning/Lec_2/Strings/practiceprob1.py
+++ day01_learning/Lec_2/Strings/practiceprob1.py
@@ -1,7 +1,7 @@
 #WAP to input users first name & print its length
 
 first_name = input("Type Your First Name : ")
-print("Your first name : ", first_name , "And length of your name = " , len(first_name))
+print("Your first name : ", first_name , "And length of your name = " , len(first_name.strip()))
 
 #WAP to find the number of occurrence of '$' in a string
 
