921e88993c8fd39e07edb8f810a99baaa4a9b2a8
ishaan83s <berealishaan@gmail.com>
2026-07-05 13:18:58 +0530

    Did a practice problem on try except and raise

---
+++ side_quests/raise custom error/practiceprob1.py
@@ -0,0 +1,15 @@
+while True:
+    a = input("Enter a number between 6 7 : , or 'quit'")
+
+    if a.lower() == "quit":
+        print("BYEEE!")
+        break
+
+    try:
+        x = float(a)
+        if x<6 or x>7:
+            raise ValueError("NOT A VALID NUMBER")
+        print(f"YOU ENTERED VALID NUMBER {x}")
+    
+    except Exception as e:
+        print(f"Error {e} occured. Please try again\n")
