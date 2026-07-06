f25167538f474f568ecfcd8cc2c9f3c821429990
ishaan83s <berealishaan@gmail.com>
2026-07-07 01:17:05 +0530

    Learnt a little csv handling , need to practice more

---
+++ side_quests/csv_file_handling/csv_file_handling.py
@@ -0,0 +1,24 @@
+import pandas as pd
+df = pd.read_csv('side_quests/csv_file_handling/student_scores.csv')
+
+#PRINTS ALL THE DATA
+print(df) #PRINTS ALL THE DATA
+
+# PRINTS FIRST 5 ROWS
+print(df.head()) #PRINTS FIRST 5 ROWS (USED for getting familiarity /info of the data)
+
+#PRINTS NUMBER OF ROWS AND COLUMNS
+print(df.shape) #prints number of rows and columns
+
+#SHOWS ALL THE COLUMNS NAMES
+print(df.columns) #shows column names
+
+# AVERAGE OF THE "marks" COLUMN
+print(df['marks'].mean()) #PRINTS average of the marks column
+
+# HOW TO FIND TOPPER (and prints its details):
+topper = df.loc[df['marks'].idxmax()]
+print(topper['name'],topper['marks'])
+
+#AVERAGE MARKS PER SUBJECT :
+print(df.groupby("subject")['marks'].mean())
