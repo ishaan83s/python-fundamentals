ccde938b28eafc53d61ee7824f4b70d764dd1db9
ishaan83s <berealishaan@gmail.com>
2026-07-09 16:55:03 +0530

    Minor changes

--- side_quests/enumerate/enumerate.py
+++ side_quests/enumerate/enumerate.py
@@ -1,4 +1,13 @@
 fruits = ["apple", "banana", "cherry"]
 
-for index, fruit in enumerate(fruits):
+for index, fruit in enumerate(fruits): #THIS MAPS EACH ELEMENT WITH ITS INDEX SAVING MORE TIME MANUALLY GETTING THE INDEX OF GETTING THE 
+#VALUE OF I IN RANGE TYPE OF THING
     print(f"Index: {index} | Fruit: {fruit}")
+
+items = ["Gold Medal", "Silver Medal", "Bronze Medal"]
+
+
+
+# Start counting from 1 instead of 0
+for rank, item in enumerate(items, start=1):
+    print(f"{rank}. {item}")
